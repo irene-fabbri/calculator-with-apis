96fd5bd37bb4c059f1cc63ace6362e446b77e865
Irene Fabbri <irenefabbri.ed@gmail.com>
2025-02-19 19:10:58 +0100

    middleware for JSON:API compliance: set content-type for responses and checking Accept header for requests

--- app.js
+++ app.js
@@ -3,43 +3,43 @@ const cors = require(`cors`);
 
 const app = express ();
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Content-Type": "application/vnd.api+json"); 
+    if(! (req.get('Accept')==="application/vnd.api+json")){ 
+        return res.status(415).send("Unsupported media type");
+    };
+    next();
+});
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
-  });
+});
 
-app.post('/add', (req,res) => {
+app.post('/sum', (req,res) => {
     // Check if body has a numbers section
-    if(!req.body.numbers) {
-        return res.status(400).send('Number list required');
-    };
-
-    if(!Array.isArray(req.body.numbers)) {
-        return res.status(400).send('Number list must be an array');
-    };
-
-    // Check if number section has at least two items
-    if(req.body.numbers.length < 2) {
-        return res.status(400).send('Need at least two numbers in the list');
+    if(!req.body.num1 || !req.body.num2) {
+        return res.status(400).send('num1 and num2 required');
     };
     
-    // If all are valid numbers, return the sum
-    let sum = 0;
-    for(let num of req.body.numbers){
-        num = parseFloat(num);
-        if(isNaN(num)){
-            return res.status(400).send('Invalid number format');
-        }
-        sum += num
+    // Check if valid numbers
+    let num1 = parseFloat(req.body.num1)
+    let num2 = parseFloat(req.body.num2)
+
+    if(isNaN(num1) || isNaN(num2)){
+        return res.status(400).send('Invalid number format');
     }
+    
+    let sum = num1 + num2    
 
     res.status(200);
     return res.json({
-        operation: "sum",
-        numbers: req.body.numbers,
-        result: `${sum}`
+        "operation": "sum",
+        "num1": `${req.body.num1}`,
+        "num2": `${req.body.num2}`,
+        "result": `${sum}`
     });
 
 });
+
