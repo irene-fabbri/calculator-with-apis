fb7f43dc1c6e22695004bad8a2cfd0fa8c9f206c
Irene Fabbri <irenefabbri.ed@gmail.com>
2025-02-19 16:44:00 +0100

    error handling in /add route

--- app.js
+++ app.js
@@ -10,11 +10,16 @@ app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
   });
 
-app.post('/add/', (req,res) => {
-    // Check if body has a number section
+app.post('/add', (req,res) => {
+    // Check if body has a numbers section
     if(!req.body.numbers) {
         return res.status(400).send('Number list required');
     };
+
+    if(!Array.isArray(req.body.numbers)) {
+        return res.status(400).send('Number list must be an array');
+    };
+
     // Check if number section has at least two items
     if(req.body.numbers.length < 2) {
         return res.status(400).send('Need at least two numbers in the list');
@@ -22,19 +27,19 @@ app.post('/add/', (req,res) => {
     
     // If all are valid numbers, return the sum
     let sum = 0;
-    try {
-        for(let num of req.body.numbers){
-            sum += parseFloat(num)
-
+    for(let num of req.body.numbers){
+        num = parseFloat(num);
+        if(isNaN(num)){
+            return res.status(400).send('Invalid number format');
         }
-        res.status(200);
-        return res.json({
-            operation: "sum",
-            numbers: req.body.numbers,
-            result: `${sum}`
-        });
-
-    } catch (error) {
-        return res.status(400).send('Invalid number format');
+        sum += num
     }
+
+    res.status(200);
+    return res.json({
+        operation: "sum",
+        numbers: req.body.numbers,
+        result: `${sum}`
+    });
+
 });
