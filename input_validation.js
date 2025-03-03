bfb0d2bc817e704afd7d9eeac2841b713a14afbf
Irene Fabbri <irenefabbri.ed@gmail.com>
2025-03-03 17:57:45 +0100

    error handling: added source pointer

--- input_validation.js
+++ input_validation.js
@@ -1,18 +1,78 @@
 module.exports = (req, res, next) => {
     const { data } = req.body;
 
-    if(!data || !data.attributes || !data.attributes.num1 || !data.attributes.num2 || Object.entries(data.attributes).length > 2) {
+    if(!data) {
         return res.status(400).send({
             "errors": [
               {
                 "status": "400",
-                "code": "invalid-parameters",          
-                "title": "num1 and num2 required",
-                "detail": "The body needs to contain the attributes num1 and num2"
+                "code": "missing-attribute",          
+                "title": "Missing Attribute",
+                "detail": "The request is missing required attributes",
+                "source" : {
+                    "pointer": "/data"
+                }
+              }
+            ]
+          });
+    } else if (!data.attributes){
+        return res.status(400).send({
+            "errors": [
+              {
+                "status": "400",
+                "code": "missing-attribute",          
+                "title": "Missing Attribute",
+                "detail": "The request is missing required attributes",
+                "source" : {
+                    "pointer": "/data/attributes/"
+                }
+              }
+            ]
+          });
+    } else if( !data.attributes.num1 ){
+        return res.status(400).send({
+            "errors": [
+              {
+                "status": "400",
+                "code": "missing-attribute",          
+                "title": "Missing Attribute",
+                "detail": "The request is missing required attributes",
+                "source" : {
+                    "pointer": "/data/attributes/num1"
+                }
+              }
+            ]
+          });
+    } else if( !data.attributes.num2 ){
+        return res.status(400).send({
+            "errors": [
+              {
+                "status": "400",
+                "code": "missing-attribute",          
+                "title": "Missing Attribute",
+                "detail": "The request is missing required attributes",
+                "source" : {
+                    "pointer": "/data/attributes/num2"
+                }
+              }
+            ]
+          });
+    } else if( Object.entries(data.attributes).length > 2){
+        return res.status(400).send({
+            "errors": [
+              {
+                "status": "400",
+                "code": "too-many-attributes",          
+                "title": "Too Many Attributes",
+                "detail": "The request has too many attributes (needed two: num1 and num2",
+                "source" : {
+                    "pointer": "/data/attributes"
+                }
               }
             ]
           });
     };
+
     // Check if valid numbers
     let num1 = parseFloat(data.attributes.num1);
     let num2 = parseFloat(data.attributes.num2);
